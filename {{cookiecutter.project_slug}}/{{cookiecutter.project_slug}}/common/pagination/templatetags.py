a7db8b8858005d375634ad55e30f5cf01362cab0
Dan Jacob <danjac2018@gmail.com>
2020-11-14 13:01:13 +0200

    Pagination

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/common/pagination/templatetags.py
@@ -0,0 +1,26 @@
+# Django
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def pagination_url(context, page_number):
+    """
+    Inserts the "page" query string parameter with the
+    provided page number into the template, preserving the original
+    request path and any other query string parameters.
+
+    Usage:
+
+    {% pagination_url 3 %}
+
+    Given the above and a URL of "/search?q=test" the result would
+    be something like:
+
+    "/search?q=test&page=3"
+    """
+    request = context["request"]
+    params = request.GET.copy()
+    params["page"] = page_number
+    return request.path + "?" + params.urlencode()
