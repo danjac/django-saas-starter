a9be7bacefe78be728e3124487708294f7382ae4
Dan Jacob <danjac2018@gmail.com>
2020-11-19 12:52:15 +0200

    Pagination context

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/common/pagination/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/common/pagination/__init__.py
@@ -21,12 +21,3 @@ def paginate(
         return paginator.page(int(request.GET.get(param, 1)))
     except (ValueError, InvalidPage):
         raise Http404(_("Invalid page"))
-
-
-def get_paginated_context(page):
-    return {
-        "page_obj": page,
-        "is_paginated": page.has_other_pages(),
-        "object_list": page.object_list,
-        "paginator": page.paginator,
-    }
