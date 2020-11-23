da54344cd58434f4fff67e6ae377df7fc0ce5b53
Dan Jacob <danjac2018@gmail.com>
2020-11-23 14:25:40 +0200

    Pagination changes

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/common/pagination/templatetags.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/common/pagination/templatetags.py
@@ -14,7 +14,7 @@ def paginate(context, object_list, **pagination_kwargs):
 
 
 @register.simple_tag(takes_context=True)
-def pagination_url(context, page_number):
+def pagination_url(context, page_number, param="page"):
     """
     Inserts the "page" query string parameter with the
     provided page number into the template, preserving the original
@@ -27,5 +27,5 @@ def pagination_url(context, page_number):
     """
     request = context["request"]
     params = request.GET.copy()
-    params["page"] = page_number
+    params[param] = page_number
     return request.path + "?" + params.urlencode()
