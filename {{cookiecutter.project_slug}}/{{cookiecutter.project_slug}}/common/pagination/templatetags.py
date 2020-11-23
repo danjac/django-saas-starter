4d61f0b6329468e658cc41ecd0167ab6b030adae
Dan Jacob <danjac2018@gmail.com>
2020-11-23 14:22:42 +0200

    Pagination changes

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/common/pagination/templatetags.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/common/pagination/templatetags.py
@@ -1,7 +1,16 @@
 # Django
 from django import template
 
+# Local
+from . import paginate as _paginate
+
 register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def paginate(context, object_list, **pagination_kwargs):
+    """Creates paginated object"""
+    return _paginate(context["request"], object_list, **pagination_kwargs)
 
 
 @register.simple_tag(takes_context=True)
