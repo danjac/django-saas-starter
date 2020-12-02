b91c85ee3284d92f2b1753ca93da029bf383b1bc
Dan Jacob <danjac2018@gmail.com>
2020-12-03 01:45:13 +0200

    Add the has_perm_or_403 shortcut

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/utils.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/utils.py
@@ -1,6 +1,16 @@
+# Django
+from django.core.exceptions import PermissionDenied
+
+
 def user_display(user):
     """
     Returns default rendering of a user. Used with the
     django_allauth user_display template tag.
     """
     return user.name or user.username
+
+
+def has_perm_or_403(user, permission, obj=None):
+    """Checks if user has permission, raises PermissionDenied otherwise"""
+    if not user.has_perm(permission, obj):
+        raise PermissionDenied
