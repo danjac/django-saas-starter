56ffe17a6c02ed0ee35a789a4037b1735753e3a6
Dan Jacob <danjac2018@gmail.com>
2020-12-26 17:22:55 +0200

    More turbo fixes

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/common/turbo/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/common/turbo/__init__.py
@@ -1,12 +1,12 @@
 # Django
 from django.template.loader import render_to_string
-from django.utils.safestring import mark_safe
 
 
-def render_turbo_stream_to_string(template, context, action, target, **template_kwargs):
-    content = render_to_string(template, context, **template_kwargs)
-    start_tag = mark_safe(
-        f'<turbo-stream target="{target}" action="{action}"><template>'
+def render_turbo_stream(target, action, content=""):
+    return f'<turbo-stream target="{target}" action="{action}"><template>{content}</template></turbo-stream>'
+
+
+def render_turbo_stream_template_to_string(template, context, action, target, **kwargs):
+    return render_turbo_stream(
+        target, action, render_to_string(template, context, **kwargs)
     )
-    end_tag = mark_safe("</template></turbo-stream>")
-    return start_tag + content + end_tag
