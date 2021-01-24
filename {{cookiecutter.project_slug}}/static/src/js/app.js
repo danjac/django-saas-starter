eb4afdc6b70788acd24338ac3d15d726b687f5a5
Dan Jacob <danjac2018@gmail.com>
2021-01-24 19:17:20 +0200

    Use esbuild

--- {{cookiecutter.project_slug}}/static/src/js/app.js
+++ {{cookiecutter.project_slug}}/static/src/js/app.js
@@ -7,10 +7,10 @@ import LinkController from './controllers/link-controller';
 import ToastController from './controllers/toast-controller';
 import ToggleController from './controllers/toggle-controller';
 
+// Stimulus setup
+const application = Application.start();
+
 application.register('confirm', ConfirmController);
 application.register('link', LinkController);
 application.register('toast', ToastController);
 application.register('toggle', ToggleController);
-
-// Stimulus setup
-const application = Application.start();
